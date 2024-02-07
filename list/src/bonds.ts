26c9ca2cc131001b90e53fdc66f4c93f5c3b67f9
hapsoa <hapsoa@gmail.com>
2024-02-08 00:45:46 +0900

    updated: bonds, reits

--- list/src/bonds.ts
+++ list/src/bonds.ts
@@ -7,4 +7,10 @@ export const bonds: AssetListItem[] = [
     desc: "미국 장기채권",
     logo: "ishares.png",
   },
+  {
+    id: "ace-us-treasury-30-h",
+    name: "ACE 미국30년국채액티브(H)",
+    desc: "미국 장기채 모은 월 배당 ETF",
+    logo: "ace.png",
+  },
 ];
--- list/src/reits.ts
+++ list/src/reits.ts
@@ -7,4 +7,21 @@ export const reits: AssetListItem[] = [
     desc: "미국 상업 부동산",
     logo: "logo.png",
   },
+  {
+    id: "sinhan-alpha-reit",
+    name: "신한알파리츠",
+    desc: "서울과 판교 오피스",
+    logo: "sinhan.png",
+  },
+  {
+    id: "miraeasset-maps-reit",
+    name: "미래에셋맵스리츠",
+    desc: "수원 광교 경기도청 오피스",
+    logo: "miraeasset.png",
+  },
+  {
+    id: "jr-global-reit",
+    name: "제이알글로벌리츠",
+    desc: "벨기에와 미국 오피스",
+  },
 ];
